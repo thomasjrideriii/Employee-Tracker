bf15276ad7a719f5623562e819dd19bce39f591b
Thomas Rider <thomasjrideriii@gmail.com>
2020-06-08 21:16:17 -0400

    Added 'add role' functionality

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ initialize = function () {
         case "View All Roles":
           viewRoles();
           break;
-        // case "Add a Role":
-        //   addRole();
-        //   break;
+        case "Add a Role":
+          addRole();
+          break;
         case "View All Departments":
           viewDepartments();
           break;
@@ -85,49 +85,113 @@ addEmployee = function () {};
 updateEmployeeRole = function () {};
 
 viewRoles = function () {
-    connection.query(
-        `SELECT title, salary, name 
+  connection.query(
+    `SELECT title, salary, name 
         FROM role
         LEFT JOIN department ON role.department_id = department.id`,
-        function (err, res) {
-            if (err) throw err;
-            console.table(res);
-            initialize();
-        }
-    )
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      initialize();
+    }
+  );
 };
 
-addRole = function () {};
+// Function for adding new Role to Database
+addRole = function () {
+  // Creating department array for inquirer
+  let departmentList = [];
+  connection.query(`SELECT name FROM department`, function (err, res) {
+    if (err) throw err;
+    for (i = 0; i < res.length; i++) {
+      departmentList.push(res[i].name);
+    }
+  });
+  // Ask user for the name of the new role, its salary, and its department
+  inquirer
+    .prompt([
+      {
+        name: "title",
+        type: "text",
+        message: "What is the name of this position?",
+      },
+      {
+        name: "salary",
+        type: "number",
+        message: "How much does this position earn per year?",
+      },
+      {
+        name: "department",
+        type: "list",
+        message: "What department does this position belong to?",
+        choices: departmentList,
+      },
+    ])
+    .then(function (result) {
+      // Empty variable for department id.
+      let departmentID = 0;
 
-viewDepartments = function () {
-    connection.query(
-        `SELECT * FROM department`,
-        function(err, res) {
-            if (err) throw err;
-            console.table(res);
-            initialize();
-        }
-    )
-};
-
-addDepartment = function () {
-    inquirer.prompt([
+      connection.query(
+        //   Searching for the department id.
+        `SELECT id FROM department WHERE ?`,
         {
-            name: "department",
-            type: "text",
-            message: "What is the name of this department?"
-        }
-    ]).then((result) => {
-        connection.query(
-            `INSERT INTO department SET ?`,
+          name: result.department,
+        },
+        function (err, res) {
+          if (err) throw err;
+          //   Set the department ID in empty variable.
+          departmentID = res[0].id;
+
+          //   Output full new role to the database
+          connection.query(
+            `INSERT INTO role SET ?`,
             {
-                name: result.department
+              title: result.title,
+              salary: result.salary,
+              department_id: departmentID,
             },
-            function(err, res) {
-                if (err) throw err;
-                console.log(result.department + " department added!");
-                initialize();
+            function (err, res) {
+              if (err) throw err;
+              console.log(result.title + " role added!");
+              //   Restart program
+              initialize();
             }
-        )
-    })
+          );
+        }
+      );
+    });
+};
+
+// Function for displaying all stored Departments
+viewDepartments = function () {
+  connection.query(`SELECT * FROM department`, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    initialize();
+  });
+};
+
+// Function for adding departments to Database
+addDepartment = function () {
+  inquirer
+    .prompt([
+      {
+        name: "department",
+        type: "text",
+        message: "What is the name of this department?",
+      },
+    ])
+    .then((result) => {
+      connection.query(
+        `INSERT INTO department SET ?`,
+        {
+          name: result.department,
+        },
+        function (err, res) {
+          if (err) throw err;
+          console.log(result.department + " department added!");
+          initialize();
+        }
+      );
+    });
 };
